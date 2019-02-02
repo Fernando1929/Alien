618f73ec14546bc3e908b8c05872221617a52901
Fernando <luis.quiles1@upr.com>
2019-02-01 21:13:31 -0400

    Changed screen dimentions

--- settings.py
+++ settings.py
@@ -2,14 +2,11 @@ class Settings():
 
 
         def __init__(self):
-            self.screen_width = 1200
-            self.screen_height = 800
+            self.screen_width = 600
+            self.screen_height = 600
             #lightblue color
             #multi color
             self.bg_color = (173,216,230)
 
             #Ship settings
             self.ship_speed_factor = 1
-    
-
-
